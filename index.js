e61c2649d0179f74a1d6e94c92d186457bd0e620
Amit biswas <amitbiswas9282@gmail.com>
2025-01-30 20:56:54 +0600

    Server setup done

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+// MiddleWare
+app.use(express.json());
+app.use(cors());
+
+// flowerShop
+// tNG8G9OrK8GR19OY
+
+const uri =
+  "mongodb+srv://<db_username>:<db_password>@cluster0.yit3t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is Running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is port ${port}`);
+});
